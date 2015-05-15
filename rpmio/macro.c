83730eb602fcc7fe9ef4067d4a5b2a45ec6130d6
Giulio Eulisse <giulio.eulisse@cern.ch>
2015-05-15 21:45:02 +0200

    Import of rpm-4.8.0-increase-macro-buffer.patch

--- rpmio/macro.c
+++ rpmio/macro.c
@@ -12,7 +12,7 @@
 
 #define	STREQ(_t, _f, _fn)	((_fn) == (sizeof(_t)-1) && rstreqn((_t), (_f), (_fn)))
 
-#define MACROBUFSIZ (BUFSIZ * 2)
+#define MACROBUFSIZ (BUFSIZ * 20)
 
 #include <rpm/rpmio.h>
 #include <rpm/rpmstring.h>
